4d201ea0d9be5539f6bf5908ec4248278489994c
iAlexT <Alextavriayt@gmail.com>
2021-05-02 10:16:56 -0300

    Create MongoClientProvider

---
+++ plugin/src/main/java/me/ialext/staff/plugin/mongo/MongoClientProvider.java
@@ -0,0 +1,12 @@
+package me.ialext.staff.plugin.mongo;
+
+import com.mongodb.client.MongoClient;
+
+import javax.inject.Provider;
+
+public class MongoClientProvider implements Provider<MongoClient> {
+
+  @Override public MongoClient get() {
+    return null;
+  }
+}
