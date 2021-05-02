56e13872919da0cfc7dfc9a611f95ab6085749c2
iAlexT <Alextavriayt@gmail.com>
2021-05-02 10:06:58 -0300

    Update User

--- api/src/main/java/me/ialext/staff/api/user/User.java
+++ api/src/main/java/me/ialext/staff/api/user/User.java
@@ -1,7 +1,23 @@
 package me.ialext.staff.api.user;
 
+import com.google.gson.annotations.SerializedName;
 import me.ialext.staff.api.model.SavableModel;
+import me.ialext.staff.api.punishment.Punishment;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface User extends SavableModel {
 
+  @SerializedName("punishments") List<Punishment> getPunishments();
+
+  default Player getPlayer() {
+    return Bukkit.getPlayer(
+        UUID.fromString(
+            getId()
+        )
+    );
+  }
 }
